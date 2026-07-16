917e652f3a64dde99a4b60a4b8dd50fea78f8f9f
dayanand-37 <dff96437@gmail.com>
2026-07-16 17:47:54 +0530

    now the user can bet the amount as he wants to bet

--- slot_machine.py
+++ slot_machine.py
@@ -1,16 +1,17 @@
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 
 def deposit():
     while True:
-        amount = input("Enter the amount you want to deposit $")
-
+        amount = input("Enter the amount you want to deposit $: ")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
                 break
             else:
-                print("Enter a valid amount.")
+                print("Amount must be greater than 0.")
         else:
             print("Enter a Number.")
 
@@ -30,14 +31,43 @@ def get_number_of_lines():
                 print("Enter lines between the given range.")
         else:
             print("Enter a number.")
-            
+
     return lines
+
+
+def get_bet():
+    while True:
+        amount = input(
+            f"Enter the amount you want to bet on each line between ${MIN_BET} - ${MAX_BET}: "
+        )
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Enter the amount between ${MIN_BET} - ${MAX_BET}")
+        else:
+            print("Enter a number.")
+
+    return amount
 
 
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance, lines)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+        if total_bet > balance:
+            print(
+                f"You do not have enough balance, your current balance is ${balance} "
+            )
+        else:
+            break
+
+    print(
+        f"You are betting ${bet} on {lines} lines and your total bet amount is ${total_bet}"
+    )
 
 
 main()
