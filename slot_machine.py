0bfa65093e40d8fe243185707feeaec069ec8bdf
dayanand-37 <dff96437@gmail.com>
2026-07-16 17:20:55 +0530

    added another feature to choose the lines the user want to bet

--- slot_machine.py
+++ slot_machine.py
@@ -1,3 +1,6 @@
+MAX_LINES = 3
+
+
 def deposit():
     while True:
         amount = input("Enter the amount you want to deposit $")
@@ -7,14 +10,34 @@ def deposit():
             if amount > 0:
                 break
             else:
-                print("Enter the amount greater than 0.")
+                print("Enter a valid amount.")
         else:
-            print("Enter a valid amount.")
+            print("Enter a Number.")
 
     return amount
 
 
+def get_number_of_lines():
+    while True:
+        lines = input(
+            "Enter the lines you want to bet between (1-" + str(MAX_LINES) + ")? "
+        )
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter lines between the given range.")
+        else:
+            print("Enter a number.")
+            
+    return lines
+
+
 def main():
     balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
+
 
 main()
