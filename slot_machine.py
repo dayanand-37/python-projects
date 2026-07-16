d322c8047a3436a343f59b61e2091768b223aab1
dayanand-37 <dff96437@gmail.com>
2026-07-16 17:02:37 +0530

    added the input feature to the slot machine

--- slot_machine.py
+++ slot_machine.py
@@ -1,1 +1,20 @@
-print("Getting started")
+def deposit():
+    while True:
+        amount = input("Enter the amount you want to deposit $")
+
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Enter the amount greater than 0.")
+        else:
+            print("Enter a valid amount.")
+
+    return amount
+
+
+def main():
+    balance = deposit()
+
+main()
