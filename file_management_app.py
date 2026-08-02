dcabe822b3969515595aab8392039abb54f3cf6f
dayanand-37 <dff96437@gmail.com>
2026-08-03 00:25:18 +0530

    added the final main function which takes input and call the other functions

--- file_management_app.py
+++ file_management_app.py
@@ -60,3 +60,45 @@ def edit_file(filename):
 
     except Exception as e:
         print("An error occured!")
+
+
+def main():
+    while True:
+        print("File management App")
+        print("1: To create a file")
+        print("2: To view all files")
+        print("3: To delete a file")
+        print("4: To read a file")
+        print("5: To edit a file a file")
+        print("6: To exit the app")
+
+        choice = input("Enter your choice between 1-6 ")
+
+        if choice == "1":
+            filename = input("Enter the file-name to create = ")
+            create_file(filename)
+
+        elif choice == "2":
+            view_all_files()
+
+        elif choice == "3":
+            filename = input("Enter the name of the file you want to delete = ")
+            delete_file(filename)
+
+        elif choice == "4":
+            filename = input("Enter the name of the file you want to read = ")
+            read_file(filename)
+
+        elif choice == "5":
+            filename = input("Enter the name of the file you want to edit = ")
+
+        elif choice == "6":
+            print("Closing the app....")
+            break
+
+        else:
+            print("Error! 'Enter your choice in the given range'")
+
+
+if __name__ == "__main__":
+    main()
