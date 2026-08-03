e87f29f000d8484853964b1a4545577c5a157f87
dayanand-37 <dff96437@gmail.com>
2026-08-04 01:12:38 +0530

    feat- user can search or count the contacts

--- contact_book.py
+++ contact_book.py
@@ -20,16 +20,20 @@ while True:
             age = input("Enter the age: ")
             email = input("Enter the Email Address: ")
             mob_num = input("Enter the mobile number: ")
-            contacts[name] = {"age": int(age), "email": email, "mobile_number": mob_num}
+            contacts[name] = {
+                "age": int(age),
+                "email": email,
+                "mobile_\ number": mob_num,
+            }
             print(f"Contact name {name} has been created successfully! ")
 
     elif choice == "2":
         name = input("Enter the name of contact you want to view: ")
         if name in contacts:
             contact = contacts[name]
-            print(f"name:{name} age:{age} mobile_number:{mob_num} email:{email}")
+            print(f"name:{name} age:{age} mobile number:{mob_num} email:{email}")
         else:
-            print(f"contact name {name} not found!")
+            print(f"contact {name} not found!")
 
     elif choice == "3":
         name = input("Enter the name of contact you want to update: ")
@@ -37,7 +41,39 @@ while True:
             age = input("Enter the updated age: ")
             email = input("Enter the updated Email Address: ")
             mob_num = input("Enter the updated mobile number: ")
-            contacts[name] = {"age": int(age), "email": email, "mobile_number": mob_num}
+            contacts[name] = {"age": int(age), "email": email, "mobile number": mob_num}
 
         else:
-            print(f"contact name {name} not found!")
+            print(f"contact {name} not found!")
+
+    elif choice == "4":
+        name = input("Enter the name of contact you want to delete: ")
+        if name in contacts:
+            del contacts[name]
+            print(f"Contact name {name} deleted successfully!")
+
+        else:
+            print(f"contact {name} not found!")
+
+    elif choice == "5":
+        search_name = input("Enter the name of contact you want to search: ")
+        found = False
+        for name, contact in contacts.items():
+            if search_name.lower() in name.lower():
+                print(
+                    f"Contact found- name:{name} age:{age} mobile number:{mob_num} email:{email}"
+                )
+                found = True
+
+            if not found:
+                print(f"contact {name} not found!")
+
+    elif choice == "6":
+        print(f"Total contacts in contact book: {len(contacts)}")
+
+    elif choice == "7":
+        print("Closing the app...")
+        break
+
+    else:
+        print("Invalid Input! Please enter the input in the given range:")
