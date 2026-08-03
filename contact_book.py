c4c61e178074af22918f3c23c660ced995881210
dayanand-37 <dff96437@gmail.com>
2026-08-04 00:52:33 +0530

    feat- now user can view or upadte any contact

--- contact_book.py
+++ contact_book.py
@@ -20,5 +20,24 @@ while True:
             age = input("Enter the age: ")
             email = input("Enter the Email Address: ")
             mob_num = input("Enter the mobile number: ")
-            contacts[name] = {"age": int(age), "email": email, "mob_num": mob_num}
+            contacts[name] = {"age": int(age), "email": email, "mobile_number": mob_num}
             print(f"Contact name {name} has been created successfully! ")
+
+    elif choice == "2":
+        name = input("Enter the name of contact you want to view: ")
+        if name in contacts:
+            contact = contacts[name]
+            print(f"name:{name} age:{age} mobile_number:{mob_num} email:{email}")
+        else:
+            print(f"contact name {name} not found!")
+
+    elif choice == "3":
+        name = input("Enter the name of contact you want to update: ")
+        if name in contacts:
+            age = input("Enter the updated age: ")
+            email = input("Enter the updated Email Address: ")
+            mob_num = input("Enter the updated mobile number: ")
+            contacts[name] = {"age": int(age), "email": email, "mobile_number": mob_num}
+
+        else:
+            print(f"contact name {name} not found!")
